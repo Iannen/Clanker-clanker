5b7895616f7aa7fb87a722f0b8513fb7346aaf59
Your Name <you@example.com>
2026-09-21 12:53:07 +0200

    ship

--- content/asset_ingestion/__init__.py
+++ content/asset_ingestion/__init__.py
@@ -1,19 +1,19 @@
-from asset_ingestion.commons.error_collector import *
-from asset_ingestion.commons.fileset_map import *
-from asset_ingestion.commons.value_extractor import *
+from .commons.error_collector import *
+from .commons.fileset_map import *
+from .commons.value_extractor import *
 
-from asset_ingestion.parsers.filelist import *
-from asset_ingestion.parsers.fileset import *
-from asset_ingestion.parsers.resolver import *
-from asset_ingestion.parsers.render import *
+from .parsers.filelist import *
+from .parsers.fileset import *
+from .parsers.resolver import *
+from .parsers.render import *
 
-from asset_ingestion.extractors.base_resolver import *
-from asset_ingestion.extractors.filelist import *
-from asset_ingestion.extractors.fileset import *
-from asset_ingestion.extractors.ui_render import *
-from asset_ingestion.extractors.domains import *
+from .extractors.base_resolver import *
+from .extractors.filelist import *
+from .extractors.fileset import *
+from .extractors.ui_render import *
+from .extractors.domains import *
 
-from asset_ingestion.assemblers.rtc import *
+from .assemblers.rtc import *
 
-from asset_ingestion.validators.assets import *
-from asset_ingestion.validators.file_list import *
+from .validators.assets import *
+from .validators.file_list import *
